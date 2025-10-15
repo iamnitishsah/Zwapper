843c7eff5628becaa3f4fe062c66d10e9a09808b
Nitish Kumar <iamnitishsah12@gmail.com>
2025-10-16 01:32:29 +0530

    Initialize Django backend with user registration API
    Set up a new Django project 'zwapper' with a 'users' app supporting user registration and JWT authentication. Added project structure, settings, requirements, and initial serializers, views, and URLs for user registration and authentication endpoints.

---
+++ Django-Backend/users/serializers.py
@@ -0,0 +1,24 @@
+from rest_framework import serializers
+from django.contrib.auth.models import User
+from django.contrib.auth.password_validation import validate_password
+
+class RegisterSerializer(serializers.ModelSerializer):
+    password = serializers.CharField(write_only=True, required=True, validators=[validate_password])
+    password2 = serializers.CharField(write_only=True, required=True)
+
+    class Meta:
+        model = User
+        fields = ['first_name', 'last_name' ,'username', 'email', 'password', 'password2']
+        extra_kwargs = {
+            'email': {'required': True},
+        }
+
+    def validate(self, data):
+        if data['password'] != data['password2']:
+            raise serializers.ValidationError("Passwords do not match")
+        return data
+
+    def create(self, validated_data):
+        validated_data.pop('password2')
+        user = User.objects.create_user(**validated_data)
+        return user
